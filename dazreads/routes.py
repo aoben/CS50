dd8773c4616ff9a5e21f849f98b747a06414948f
aoben <ayuoben@gmail.com>
2019-07-17 07:02:40 -0500

    GoodReads_API Call and  DazReads API implemented

--- dazreads/routes.py
+++ dazreads/routes.py
@@ -1,8 +1,11 @@
-from flask import render_template, url_for, flash, redirect, request
+from flask import render_template, url_for, flash, redirect, request, jsonify
 from dazreads import app, db, bcrypt
 from dazreads.forms import RegistrationForm, LoginForm, SearchForm, ReviewForm
 from dazreads.models import User, Post, Books, Reviews, Rates  #this is put here to avoid a circular error...so this runs after db = SQLAlchemy(app)
 from flask_login import login_user, current_user, logout_user, login_required
+from sqlalchemy import text, func
+import requests
+
 
 
 
@@ -74,13 +77,25 @@ def search():
        
         return render_template('search.html', title = 'Search', form = form, posts = books)
 
+
+
 @app.route("/bookpage/<book_id>", methods=['GET', 'POST'])
 @login_required
 def bookpage(book_id):
+        
         books = Books.query.get_or_404(book_id)
         form = ReviewForm()
         review = Reviews.query.filter_by(book_id = book_id).all() #This is the get portion to display all reviews for the book
+        count_rate = Rates.query.filter_by(book_id = book_id).count()
+        avg_rate = round(float(db.session.query(func.avg(Rates.rating).label('average')).filter_by(book_id=book_id).scalar()), 2)
         
+        #API call to Goodreads
+        url = 'https://www.goodreads.com/book/review_counts.json?isbns=' + book_id
+        res = requests.get(url)
+        goodreads = res.json()
+        average_rating = goodreads["books"][0]["average_rating"]
+        work_ratings_count = goodreads["books"][0]["work_ratings_count"]
+
         if form.validate_on_submit():
                 rate_field = form.rate.data
                 review_field = form.content.data
@@ -105,7 +120,7 @@ def bookpage(book_id):
                                         db.session.commit()
                                         flash('Review has been submitted!', 'success')
 
-        return render_template('bookpage.html', title=books.title, form = form, posts = books, review = review)
+        return render_template('bookpage.html', title=books.title, form = form, posts = books, review = review, avg_rate=avg_rate, count_rate=count_rate, goodreads=goodreads)
 
 
 @app.route("/logout")
@@ -117,3 +132,25 @@ def logout():
 @login_required
 def account():
         return render_template('account.html', title = 'Account')
+
+
+@app.route("/api/<book_id>")
+@login_required
+def books_api(book_id):
+        book = Books.query.filter_by(isbn=book_id).first()
+        if book is None:       
+                return jsonify({"error": "Invalid Book_id"}), 422
+        average_score = round(float(db.session.query(func.avg(Rates.rating).label('average')).filter_by(book_id=book_id).scalar()), 2)
+        review_count = Reviews.query.filter_by(book_id=book_id).count()
+        title = book.title
+        author = book.author
+        year = book.year
+        isbn = book.isbn
+        return jsonify({
+                "title":title, 
+                "author":author,
+                "year":year,
+                "isbn":isbn,
+                "review_count":review_count,
+                "average_score":average_score
+        })
